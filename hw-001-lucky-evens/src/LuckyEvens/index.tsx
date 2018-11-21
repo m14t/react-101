640c6fbc972e98f7b30b7075cd361adb053c2d39
Matt Farmer <matt.farmer@wizeline.com>
2018-11-20 17:00:11 -0800

    Add game logic

--- hw-001-lucky-evens/src/LuckyEvens/index.tsx
+++ hw-001-lucky-evens/src/LuckyEvens/index.tsx
@@ -5,9 +5,27 @@ interface LuckyEvensProps {
   title: string;
 }
 
-class LuckyEvens extends PureComponent<LuckyEvensProps, {}> {
+interface LuckyEvensState {
+  gamesPlayed: number;
+  gamesWon: number;
+}
+
+class LuckyEvens extends PureComponent<LuckyEvensProps, LuckyEvensState> {
   static defaultProps = {
     title: 'Lucky Evens',
+  };
+
+  state = {
+    gamesPlayed: 0,
+    gamesWon: 0,
+  };
+
+  onRoll = (value: number) => {
+    const isWinner = value % 2 === 0;
+    this.setState((state) => ({
+      gamesPlayed: state.gamesPlayed + 1,
+      gamesWon: state.gamesWon + (isWinner ? 1 : 0),
+    }));
   };
 
   render() {
@@ -15,7 +33,17 @@ class LuckyEvens extends PureComponent<LuckyEvensProps, {}> {
       <div>
         <h1>{this.props.title}</h1>
 
-        <Dice />
+        <Dice onRoll={this.onRoll} />
+
+        <div>
+          <strong>Games Played:</strong>
+          {this.state.gamesPlayed}
+        </div>
+
+        <div>
+          <strong>Games Won:</strong>
+          {this.state.gamesWon}
+        </div>
       </div>
     );
   }
